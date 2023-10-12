451ed5329ab76fa6eef56ea9f3b8f62b49145626
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-12 19:24:05 +0700

    Add files via upload

---
+++ Intro to Internet Technology/Week9/src/components/Contact.js
@@ -0,0 +1,23 @@
+const contacts = [
+    {
+      name: "Bocchi",
+      imgURL: "https://i.pinimg.com/564x/12/fd/40/12fd40fb660fa646b79fbab084b856ad.jpg",
+      phone: "+123 456 789",
+      email: "hitorigotoh@gmail.com",
+    },
+    {
+      name: "Bronya",
+      imgURL: "https://i.pinimg.com/564x/4d/73/a1/4d73a12055d6de7831b5b85093492f53.jpg",
+      phone: "+987 654 321",
+      email: "Bronya123@gmail.com",
+    },
+    {
+      name: "Nilou",
+      imgURL: "https://i.pinimg.com/564x/79/77/af/7977af598870c9b82803de1f739fa016.jpg",
+      phone: "+62 145 513 414",
+      email: "Nilou321@gmail.com",
+    },
+  ];
+  
+  export default contacts;
+  
